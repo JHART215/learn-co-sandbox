762b3b7c03b79bca929da5a7906d64d50348d581
Jeremy Hart <jerome215@comcast.net>
2020-07-15 14:31:27 +0000

    Automatically backed up by Learn

---
+++ learning-comments.rb
@@ -0,0 +1,9 @@
+# Don't do this:
+
+puts ( 1 #+ 1)
+
+# From the Three Dog Night song: "Joy to the World (Jeremiah was a Bullfrog)"
+puts "Joy to the world"
+puts "All the boys and girls"
+# "Joy to the fishes in the deep blue sea"
+puts "Joy to you and me"
